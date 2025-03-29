db173e5561d2b522ef7083e03cc0dde515245703
Isidro Santiago <santiagoissidro@gmail.com>
2025-03-28 22:33:25 -0600

    Add AWS S3 configuration and service for file storage and retrieval

---
+++ backend/src/main/java/com/isidrosantiago/backend/s3/S3Config.java
@@ -0,0 +1,34 @@
+package com.isidrosantiago.backend.s3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        S3Client client = S3Client.builder()
+            .region(Region.of(region))
+            .credentialsProvider(StaticCredentialsProvider.create(credentials))
+            .build();
+
+        return client;
+    }
+}
---
+++ backend/src/main/java/com/isidrosantiago/backend/s3/S3Service.java
@@ -0,0 +1,58 @@
+package com.isidrosantiago.backend.s3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+
+import static software.amazon.awssdk.services.s3.model.ObjectCannedACL.PUBLIC_READ;
+
+@Service
+public class S3Service {
+
+    @Autowired
+    private S3Client s3;
+
+    @Value("${aws.bucket}")
+    private String bucket;
+
+    @Value("${aws.region}")
+    private String region;
+
+    public String storeObject(String filename, byte[] file) {
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+            .bucket(bucket)
+            .key("java-test/" + filename)
+            .acl(PUBLIC_READ)
+            .build();
+
+        s3.putObject(objectRequest, RequestBody.fromBytes(file));
+        return getPublicUrl(filename);
+    }
+
+    public byte[] getObject(String filename) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+            .bucket(bucket)
+            .key(filename)
+            .build();
+
+        ResponseInputStream<GetObjectResponse> response = s3.getObject(getObjectRequest);
+
+        try {
+            return response.readAllBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private String getPublicUrl(String filename) {
+        return "https://" + bucket + ".s3." + region + ".amazonaws.com/" + filename;
+    }
+}
